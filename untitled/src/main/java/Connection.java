59aa3bc887d90836ff3405cf5df8e98bebeaf139
nurbeyza <47194723+nurbeyza@users.noreply.github.com>
2022-07-22 01:37:45 +0300

    yeni

---
+++ untitled/src/main/java/Connection.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+//Bu sınıf tüm gerekli path'leri verdiğimiz ChromeDriver'a bağlantı sağladığımız fonksiyonu barındıran sınıf
+public class Connection {
+
+    public static WebDriver driver;
+
+
+    public WebDriver Browser() {
+        //ChromeDriver.exe yolu ve gerekli url veriliyor
+        Path resourceDirectory = Paths.get("src","main","resources");
+        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+        System.setProperty("webdriver.chrome.driver", absolutePath+"/binary/chromedriver.exe");
+        String url = "https://demoqa.com/webtables";
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //get methodu ile ilgili url'nin yönlendirilmesi yapılıyor
+        driver.get(url);
+
+        return driver;
+    }
+}
