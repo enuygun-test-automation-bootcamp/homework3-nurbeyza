0a4b599500897c6a039788743088d6ce33349e16
nurbeyza <47194723+nurbeyza@users.noreply.github.com>
2022-07-24 02:19:10 +0300

    yeni commit

--- untitled/src/main/java/Browser.java
+++ untitled/src/main/java/Browser.java
@@ -2,12 +2,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+/**
+ * *Bu sınıf Connection sınıfından miras almıştır.
+ * *Connection sınıfında bulunan url ve path fonksiyonlarını rahatlıkla kullanabilir.
+ */
+
 public class Browser extends Connection{
     public static WebDriver driver;
 
+    //Bu fonksiyon açılmak istenilen browserın ne olduğu anlayıp URL ve driiver yolunu alarak ilgili sayfayı açıyor
+    //Fonksiyon driver döndürüyor
     public WebDriver OpenBrowser(String Browser, String url, String path) {
         if(Browser.equals("Chrome")){
-
             System.setProperty("webdriver.chrome.driver", path+ "/binary/chromedriver.exe");
             driver = new ChromeDriver();
             driver.get(url);
@@ -15,18 +22,14 @@ public class Browser extends Connection{
             return driver;
         }
         else if (Browser.equals("Firefox")) {
-
             System.setProperty("webdriver.gecko.driver", path+ "/binary/geckodriver.exe");
             driver = new FirefoxDriver();
             driver.manage().window().maximize();
             driver.get(url);
-
             return driver;
         }
-
         return null;
     }
-
 
 
 }
--- untitled/src/main/java/Connection.java
+++ untitled/src/main/java/Connection.java
@@ -7,16 +7,13 @@ import java.nio.file.Paths;
 
 //Bu sınıf tüm gerekli path'leri verdiğimiz ChromeDriver'a bağlantı sağladığımız fonksiyonu barındıran sınıf
 public class Connection{
-
-
     public String url() {
         //ChromeDriver.exe yolu ve gerekli url veriliyor
         String url = "https://www.enuygun.com/";
         return url;
     }
-
     public String path(){
-        Path resourceDirectory = Paths.get("untitled","src","main","resources");
+        Path resourceDirectory = Paths.get("src","main","resources");
         String absolutePath = resourceDirectory.toFile().getAbsolutePath();
         return absolutePath;
     }
